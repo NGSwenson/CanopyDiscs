39a9bf67f06fa307c8c93170fb3797a278c31929
Nate Swenson <swenson@umd.edu>
2018-08-22 20:42:37 -0400

    Commenting file

--- Jaccard.Bray.Observed.R
+++ Jaccard.Bray.Observed.R
@@ -3,14 +3,14 @@ library(picante)
 
 allom = read.csv("serc_allometry.csv")
 
-sec = read.csv("serc.second.mat.csv",row.names=1)
+sec = read.csv("serc.second.mat.csv",row.names=1) #rows individuals cols species
 sec[is.na(sec)] = 0
 
 
 tmp = as.data.frame(allom[,"Mnemonic"])
 tmp2 = cbind(rownames(allom),tmp)
-colnames(tmp2) = c("num","sp")
-spp = split(tmp2,tmp2$sp)
+colnames(tmp2) = c("num","sp") #making a dataframe with first col is rowname indicator and second is species name
+spp = split(tmp2,tmp2$sp) #splitting by species
 
 beta1.out = matrix(NA, ncol = 4, nrow=length(spp))
 beta2.out = matrix(NA, ncol = 4, nrow=length(spp))
@@ -18,19 +18,19 @@ beta2.out = matrix(NA, ncol = 4, nrow=length(spp))
 
 for(i in 1:length(spp)){
 	print(i)
-	ind.mat = sec[spp[[i]][,1]  , -i]
-	jac.out = vegdist(ind.mat,method="jaccard")
-	bray.out = vegdist(ind.mat,method="bray")
+	ind.mat = sec[spp[[i]][,1]  , -i] # get data matrix for all individuals of species i excluding own species
+	jac.out = vegdist(ind.mat,method="jaccard") # calculate jaccard 
+	bray.out = vegdist(ind.mat,method="bray") # calculate bray
 
-	beta1.out[i,1] = nrow(ind.mat)
-	beta1.out[i,2] = mean(jac.out,na.rm=T)
-	beta1.out[i,3] = median(jac.out,na.rm=T)
-	beta1.out[i,4] = sd(jac.out,na.rm=T)
+	beta1.out[i,1] = nrow(ind.mat) #number of individuals of species i
+	beta1.out[i,2] = mean(jac.out,na.rm=T) # mean jaccard between heterospecific neighborhoods
+	beta1.out[i,3] = median(jac.out,na.rm=T) # median jaccard between heterospecific neighborhoods
+	beta1.out[i,4] = sd(jac.out,na.rm=T) # st dev jaccard between heterospecific neighborhoods
 	
-	beta2.out[i,1] = nrow(ind.mat)
-	beta2.out[i,2] = mean(bray.out,na.rm=T)
-	beta2.out[i,3] = median(bray.out,na.rm=T)
-	beta2.out[i,4] = sd(bray.out,na.rm=T)
+	beta2.out[i,1] = nrow(ind.mat) #number of individuals of species i
+	beta2.out[i,2] = mean(bray.out,na.rm=T) # mean bray between heterospecific neighborhoods
+	beta2.out[i,3] = median(bray.out,na.rm=T) # median bray between heterospecific neighborhoods
+	beta2.out[i,4] = sd(bray.out,na.rm=T) # st dev bray between heterospecific neighborhoods
 }
 
 rownames(beta1.out) = names(spp)
