dfc74f171107702c039ddcdf9dd8a1a08e22a49b
Nate Swenson <swenson@umd.edu>
2018-08-22 20:47:06 -0400

    Commenting on Code

--- Jaccard.Randomizations.R
+++ Jaccard.Randomizations.R
@@ -1,3 +1,6 @@
+##NG Swenson (swenson@umd.edu)
+##22 August 2018
+
 setwd("/Users/nateswenson/data/ctfs/jenny.overlap/output")
 library(picante)
 
@@ -11,11 +14,15 @@ tmp = as.data.frame(allom[,"Mnemonic"])
 tmp2 = cbind(rownames(allom),tmp)
 colnames(tmp2) = c("num","sp")
 
+# a function that calculates jaccard distance between heterospecific neighborhoods
+# lapply'd to list
+# performs randomization of species names (i.e. completely randomized forest)
 rand.jac = function(xx){
 
 	xx$num = sample(xx$num)
 	spp = split(xx,xx$sp)
 
+	#generates species level community data matrix (CDMs)
 	com.list = list()
 		for(i in 1:length(spp)){
 		nam = names(spp)[i]
@@ -31,17 +38,18 @@ rand.jac = function(xx){
 		output = cbind(mean.jacs,median.jacs,sd.jacs)
 		return(output)
 	}
-	return(lapply(com.list, jac.fun))
+	return(lapply(com.list, jac.fun)) #applying across list of species neighborhood CDMs
 	
 	}
-	
+
+#replicating the rand.jac function 999 times each time with a random forest
 rand.output = replicate(999,rand.jac(tmp2))
 
 means = matrix(NA, nrow=nrow(rand.output),ncol=999)
 medians = matrix(NA, nrow=nrow(rand.output),ncol=999)
 sds = matrix(NA, nrow=nrow(rand.output),ncol=999)
 
-
+#summarizing output
 for(i in 1:nrow(rand.output)){
 	for(j in 1:999){
 	means[i,j] =	rand.output[i,][[j]][1,1]
@@ -49,5 +57,11 @@ for(i in 1:nrow(rand.output)){
 	sds[i,j] =	rand.output[i,][[j]][1,3]	
 	}
 }
+
+
+
+write.table(means,"serc.jac.rand.means.csv",sep=",",row.names=F,col.names=T,quote=F)
+write.table(medians,"serc.jac.rand.medians.csv",sep=",",row.names=F,col.names=T,quote=F)
+write.table(sds,"serc.jac.rand.sds.csv",sep=",",row.names=F,col.names=T,quote=F)
 
 
